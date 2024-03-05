5512b08b8e6aac1790622be86bec1318062aaa44
BigPhatNerd <wilsonhorrell@gmail.com>
2024-03-05 12:22:17 -0600

    Added Page not found

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const routes = require('./routes')
 
+app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(routes)
+app.use(routes);
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`)
